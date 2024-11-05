ea7c20643e18361c99dcf28c90086f0b8e5583b6
thomas-virtual <dev_thomas@virtualloja.com.br>
2024-11-05 17:59:46 -0300

    Tela de lista de eventos e Logica para filtrar os eventos

--- Front/ProEventos-APP/src/app/app.module.ts
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEvents: any = [];
+  private _listFilter: string = '';
+
+  public isToggled: boolean = false;
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +20,35 @@ export class EventosComponent implements OnInit {
     this.getEventos()
   }
 
+  public get listFilter(): string {
+    return this._listFilter
+  }
+
+  public set listFilter(filter: string) {
+    this._listFilter = filter
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.eventos
+  }
+
+  filterEvents(filterFor: string): any {
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string, local: string}) => {
+        return evento.tema.toLocaleLowerCase().indexOf(filterFor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterFor) !== -1
+      }
+    )
+  }
+
+  public alterarImagem(): void {
+    this.isToggled = !this.isToggled;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.filteredEvents = this.eventos
+      },
       error => console.log(error),
     )
   }
